28f137b02efa4f5d2c23fe439076a8eef78cdb26
Nikil <nikilvd1985@gmail.com>
2021-04-22 16:52:47 +0530

    tabbar and webview

---
+++ IHMA/Views-2nd page/ViewController2.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController2.swift
+//  IHMA
+//
+//  Created by Nikil Davis on 22/04/21.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+
+class ViewController2: UIViewController {
+    
+    @IBOutlet weak var usageIndicator: UIActivityIndicatorView!
+    
+
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var lbl: UILabel!
+    
+    override func viewDidLoad() {
+        
+        usageIndicator.isHidden = false
+        usageIndicator.startAnimating()
+        super.viewDidLoad()
+        lbl.text = "Second Page"
+        
+        // Your webView code goes here
+           let url = URL(string: "https://www.google.com")
+           let requestObj = URLRequest(url: url! as URL)
+       webView.load(requestObj)
+        usageIndicator.stopAnimating()
+        usageIndicator.isHidden = true
+        
+         }
+
+}
